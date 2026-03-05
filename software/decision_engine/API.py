5ab2186df85605e2745e3d26e1ec6ec20c4f6013
Pi-0909 <pobowater6767@gmail.com>
2026-03-05 09:57:09 +0100

    fixed api to return hourly percipitation value

--- software/decision_engine/API.py
+++ software/decision_engine/API.py
@@ -1,30 +1,24 @@
 import requests
 import time
 
-def get_total_precipitation_data(api_key):
-    url = f"http://api.weatherapi.com/v1/forecast.json?key=dff7a83506cc4f1bbb6111706260202&q=auto:ip&days=2&aqi=no&alerts=yes"
+def get_hourly_precipitation_data(api_key):
+    url = f"http://api.weatherapi.com/v1/forecast.json?key={api_key}&q=auto:ip&days=2&aqi=no&alerts=no"
     response = requests.get(url)
 
     if response.status_code == 200:
         forecast_data = response.json()
-        total_precipitation = 0
 
         for day in forecast_data['forecast']['forecastday']:
-            total_precipitation += day['day'].get('totalprecip_mm', 0)
-            total_precipitation += day['day'].get('totalsnow_cm', 0) * 10
+            for hour in day['hour']:
+                precip = hour.get('precip_mm', 0)
+                print(f" {precip}")
 
-        return total_precipitation
     else:
         print(f"Error: {response.status_code} - {response.text}")
-        return None
 
 if __name__ == "__main__":
     api_key = 'dff7a83506cc4f1bbb6111706260202'
 
     while True:
-        total_precipitation = get_total_precipitation_data(api_key)
-
-        if total_precipitation is not None:
-            print(total_precipitation, "mm")
-
+        get_hourly_precipitation_data(api_key)
         time.sleep(3600)
